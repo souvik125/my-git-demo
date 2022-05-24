3d854cab484aedcb15c8c535a438d0e30358f744
souvik125 <souvikdas500@gmail.com>
2022-05-24 14:00:36 +0530

    initial commit

---
+++ src/com/cts/Main.java
@@ -0,0 +1,11 @@
+package com.cts;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Session");
+		System.out.println("Hope you are able to follow");
+	}
+
+}
