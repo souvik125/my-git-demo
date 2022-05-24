2e110222bd8cbc8d27d95ddf45b63595237f8e33
souvik125 <souvikdas500@gmail.com>
2022-05-24 15:14:27 +0530

    first branch 'iss1' commit

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,11 +1,16 @@
 package com.cts;
 
+import com.cts.service.SomeService;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Session");
 		System.out.println("Hope you are able to follow");
+		
+		SomeService ss = new SomeService();
+		System.out.println(ss.findLength("Souvik"));
 	}
 
 }
---
+++ src/com/cts/service/SomeService.java
@@ -0,0 +1,7 @@
+package com.cts.service;
+
+public class SomeService {
+	public int findLength(String str) {
+		return str.length();
+	}
+}
